54d9d2a853224a59879dc28bf5b0af29fcf52103
Marcos Vasconcellos De Andrade <04261040727@dccbra.ifb.local>
2019-10-17 21:07:30 -0300

    ifb17out19

---
+++ Atividade17-10 ex2.sql
@@ -0,0 +1,14 @@
+-- use sua criatividade para criar um DER com uma única entidade
+-- que contenha no mínimo 10 atributos
+-- crie um banco de dados para o DER da questão 1
+CREATE TABLE alimenticios (
+    ord
+    codigo
+    tipo
+    nome
+    quantidade
+    unidade
+    
+    valor
+
+)
